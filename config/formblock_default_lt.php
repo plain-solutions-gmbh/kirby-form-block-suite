82165c6c41cef7a40d1791e863659cbd6bb71d04
romanustin <53936318+romanustin@users.noreply.github.com>
2023-01-05 12:19:34 +0200

    Create formblock_default_lt.php

---
+++ config/formblock_default_lt.php
@@ -0,0 +1,37 @@
+[{
+    "content": {
+        "formfields": [{
+                "content": {
+                    "label": "Vardas",
+                    "slug": "name",
+                    "autofill": "name",
+                    "required": true,
+                    "inputtype": "text"
+                },
+                "isHidden": false,
+                "type": "formfields_01_input"
+            },
+            {
+                "content": {
+                    "label": "El. paštas",
+                    "slug": "email",
+                    "autofill": "email",
+                    "required": true,
+                    "inputtype": "email",
+                    "validate": [{
+                        "validate": "email"
+                    }]
+                },
+                "isHidden": false,
+                "type": "formfields_01_input"
+            }
+        ],
+        "name": "Forma",
+        "enable_notify": false,
+        "enable_confirm": false,
+        "redirect": false,
+        "display": "{{name}} ({{email}})"
+    },
+    "isHidden": false,
+    "type": "form"
+}]
