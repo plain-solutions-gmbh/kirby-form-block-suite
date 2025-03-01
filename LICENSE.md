76682f45e96ab95f0b447ba1c086876d0647f59a
Roman Gsponer <mail@microman.ch>
2025-03-01 17:21:36 +0100

    Replace SymLinks

--- LICENSE.md
+++ LICENSE.md
@@ -1,1 +1,34 @@
-/Users/romangsponer/Cloud/_kirby/licenses/PLAIN/LICENSE.md
+Plain Solutions Software License  
+Version 1.1, February 2025  
+
+Copyright (c) 2025 Plain Solutions GmbH  
+All rights reserved.  
+
+## 1. Grant of License  
+This software is provided under GPL-3.0-only license. You are granted a non-exclusive, non-transferable, and revocable right to download, install, and use the software **for non-commercial purposes** only.  
+
+## 2. Permitted Use  
+- You may download and use the software for personal, educational, or research purposes.  
+- You may modify the software **for personal use only** but may not distribute modified versions.  
+
+## 3. Restrictions  
+- Commercial use, resale, or redistribution of the software in any form is **strictly prohibited**.  
+- You may not sublicense, rent, lease, or sell copies of this software.  
+- Decompiling, reverse engineering, or modifying the software for redistribution is not allowed.  
+
+## 4. No Warranty & Limitation of Liability  
+This software is provided "as is" without any warranties. Plain Solutions GmbH is not responsible for any damages, data loss, or other issues arising from the use of this software.  
+
+## 5. Support & Updates  
+Support and updates may be provided at the sole discretion of Plain Solutions GmbH. There is no guarantee of future updates or bug fixes.  
+
+## 6. Termination  
+Plain Solutions GmbH reserves the right to revoke this license if the terms are violated. Upon termination, you must stop using the software and delete all copies.  
+
+## 7. Governing Law  
+This license is governed by Swiss law. Any disputes shall be settled in the jurisdiction of Plain Solutions GmbH’s registered office in Raron, Switzerland.  
+
+For inquiries, please contact:  
+Plain Solutions GmbH  
+Theaterstrasse 2, 3942 Raron, Switzerland  
+Email: support@plain-solutions.net  
---
+++ utils/Plugin.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Plain\Helpers;
+
+/**
+ * @package   Kirby Plain Helpers
+ * @author    Roman Gsponer <support@plain-solutions.net>
+ * @link      https://plain-solutions.net/
+ * @copyright Roman Gsponer
+ * @license   https://plain-solutions.net/terms/ 
+ */
+
+use Kirby\Cms\App;
+use Kirby\Data\Data;
+
+class Plugin
+{
+
+    const CLASSES = [
+        'Plain\\Helpers\\License' => __DIR__ . '/License.php',
+        'Plain\\Helpers\\Autoloader' => __DIR__ . '/Autoloader.php',
+    ];
+
+    public static function load(
+        string $name,
+        ?array $extends = null,
+        ?array $info = null,
+        string|null $root = null,
+        Autoloader|array|bool $autoloader = false
+    ): void {
+
+        spl_autoload_register([self::class, 'autoload']);
+
+        $root ??= dirname(debug_backtrace()[0]['file']);
+        $info ??= Data::read($root . '/composer.json');
+
+        //Needs to be loadet before autoload!
+        $license_obj = ($info['license'] === 'MIT') ? null : new License($name, $info);
+
+        $extends = Autoloader::load(
+            name:   $name,
+            root:   $root,
+            data:   $extends ?? [],
+            tasks:  $autoloader
+        );
+
+        $params = [
+            'name'      => $name,
+            'info'      => $info,
+            'root'      => $root
+        ];
+        
+        //Kirby > 5.0.0 allows license status
+        if (version_compare(App::version() ?? '0.0.0', '4.9.9', '>')) {
+
+            if ($license_obj === null) {
+                $status = static::licenseArray($info);
+            }
+
+            $params['license'] = [
+                'name'      => $info['license'],
+                'status'    => $status ?? $license_obj?->licenseArray($info)
+            ];
+        }
+        
+        $params['extends'] = $license_obj?->extends($extends) ?? $extends;
+
+        App::plugin(...$params);
+    }
+
+    private static function licenseArray($info): ?array
+    {
+        if ($donate = $info['funding'][0]['url'] ?? null) {
+            return  [
+                'value'     => 'active',
+                'link'      => $donate,
+                'theme'     => 'pink',
+                'label'     => 'Donate',
+                'icon'      => 'heart',
+            ];
+        }
+        return null;
+    }
+
+    private static function autoload(string $className): void
+    {
+        if (array_key_exists($className, static::CLASSES)) {
+            require_once static::CLASSES[$className];
+            return;
+        }
+    }
+
+}
